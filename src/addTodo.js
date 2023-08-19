0119103cf00224247d058e0681d1f42a5f552980
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2023-08-19 15:58:40 +0530

    Added addTodo.js and updated createProject.js

---
+++ src/addTodo.js
@@ -0,0 +1,28 @@
+import createProject from "./createProject";
+import createTodo from "./createTodo";
+
+export default function addTodo(projectName) {
+  var projects = JSON.parse(localStorage.getItem("todo"));
+
+  if (projects[projectName] === undefined) {
+    createProject(projectName);
+    projects = JSON.parse(localStorage.getItem("todo"));
+  }
+  const title = document.getElementById("title");
+  const description = document.getElementById("description");
+  const dueDate = document.getElementById("dueDate");
+  const priority = document.getElementById("priority");
+
+  projects[projectName].push(
+    createTodo(
+      projects[projectName].length,
+      title.value,
+      description.value,
+      dueDate.value,
+      priority.options[priority.selectedIndex].value
+    )
+  );
+  localStorage.setItem("todo", JSON.stringify(projects));
+
+  console.log(projects);
+}
--- src/createProject.js
+++ src/createProject.js
@@ -1,3 +1,11 @@
-export default function projects(name) {
-  return (window[name] = []);
+export default function createProject(projectName) {
+  var projects = JSON.parse(localStorage.getItem("todo"));
+  if (localStorage.getItem("todo") === null) {
+    let newProject = {};
+    newProject[projectName] = [];
+    localStorage.setItem("todo", JSON.stringify(newProject));
+  } else if (projects[projectName] === undefined) {
+    projects[projectName] = [];
+    localStorage.setItem("todo", JSON.stringify(projects));
+  }
 }
