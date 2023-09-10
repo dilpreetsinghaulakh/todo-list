04218cfd9601fe080138ea050e18c35376c0f5bf
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2023-09-10 20:30:26 +0530

    Added weekView.

--- src/ui.js
+++ src/ui.js
@@ -164,7 +164,7 @@ export default class ui {
         {
           name: "This Week",
           view: function () {
-            console.log("to be made...");
+            weekView();
           },
           svg: `<svg class="w-6 h-6" width="24" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="Calendar / Calendar_Week"><path id="Vector" d="M4 8H20M4 8V16.8002C4 17.9203 4 18.4801 4.21799 18.9079C4.40973 19.2842 4.71547 19.5905 5.0918 19.7822C5.5192 20 6.07899 20 7.19691 20H16.8031C17.921 20 18.48 20 18.9074 19.7822C19.2837 19.5905 19.5905 19.2842 19.7822 18.9079C20 18.4805 20 17.9215 20 16.8036V8M4 8V7.2002C4 6.08009 4 5.51962 4.21799 5.0918C4.40973 4.71547 4.71547 4.40973 5.0918 4.21799C5.51962 4 6.08009 4 7.2002 4H8M20 8V7.19691C20 6.07899 20 5.5192 19.7822 5.0918C19.5905 4.71547 19.2837 4.40973 18.9074 4.21799C18.4796 4 17.9203 4 16.8002 4H16M8 4H16M8 4V2M16 4V2M16 12H8" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></g></svg>`,
         },
@@ -1307,6 +1307,30 @@ export default class ui {
       let sorted = _.orderBy(
         _.filter(todoOnly, (element) => {
           return element.dueDate < new Date().toJSON().slice(0, 10);
+        }),
+        ["priority", "title"],
+        ["desc", "asc"]
+      );
+      getViewUi(sorted, "Past");
+    };
+
+    const weekView = () => {
+      const todoOnly = this.getTodoOnly();
+
+      Date.prototype.getWeek = function () {
+        var oneJan = new Date(this.getFullYear(), 0, 1);
+        var today = new Date(
+          this.getFullYear(),
+          this.getMonth(),
+          this.getDate()
+        );
+        var dayOfYear = (today - oneJan + 86400000) / 86400000;
+        return Math.ceil(dayOfYear / 7);
+      };
+
+      let sorted = _.orderBy(
+        _.filter(todoOnly, (element) => {
+          return new Date(element.dueDate).getWeek() === new Date().getWeek();
         }),
         ["priority", "title"],
         ["desc", "asc"]
